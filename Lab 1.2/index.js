379375eb6405f8b4bc1a2cdb0c8cdb1ccabcec3f
102560056 <102560056@student.swin.edu.au>
2024-02-28 04:49:43 +1100

    Lab1 DONE

---
+++ Lab 1.2/index.js
@@ -0,0 +1,24 @@
+const buttons = document.getElementsByTagName("button");
+const figures = document.getElementsByTagName("figure");
+var activeButton = ''
+
+
+function OnClick(button) {
+    for (var i = 0; i < buttons.length; i++) {
+        if (buttons[i] == button) {
+            button.classList.toggle("Active")
+            activeButton = activeButton == button.innerHTML ? '' : button.innerHTML
+        }
+        else {
+            buttons[i].classList.remove("Active");
+            figures[i].display
+        }
+    }
+
+    for (var i = 0; i < figures.length; i++) {
+        figures[i].style.display = "none";
+    }
+
+    document.getElementById(activeButton).style.display = "block";
+
+}
